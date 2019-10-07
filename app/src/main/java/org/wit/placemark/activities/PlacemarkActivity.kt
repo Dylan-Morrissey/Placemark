21b3136b593516d3b07212aa9f3147adf9267868
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2019-10-07 12:16:30 +0100

    added the ability to upload Images with placemark. Lab4

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -1,25 +1,97 @@
-package com.example.placemark
+package org.wit.placemark.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activityplacemark.*
+import android.provider.SyncStateContract.Helpers.update
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_placemark.*
+import kotlinx.android.synthetic.main.activity_placemark_list.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.startActivityForResult
 import org.jetbrains.anko.toast
+import org.wit.placemark.R
+import org.wit.placemark.helpers.readImage
+import org.wit.placemark.helpers.readImageFromPath
+import org.wit.placemark.helpers.showImagePicker
+import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.PlacemarkStore
+import org.wit.placemark.models.PlacemarkModel
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activityplacemark)
-    info("Placemark Activity started.")
-    btnAdd.setOnClickListener() {
-      val placemarkTitle = placemarkTitle.text.toString()
-      if (placemarkTitle.isNotEmpty()) {
-        info("add Button Pressed: $placemarkTitle")
-      }
-      else {
-        toast ("Please Enter a title")
-      }
+
+    var placemark = PlacemarkModel()
+    lateinit var app: MainApp
+    val IMAGE_REQUEST = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placemark)
+        info("Placemark Activity started..")
+
+        app = application as MainApp
+        toolbarAdd.title = title
+
+        var state = 0;
+
+        setSupportActionBar(toolbarAdd)
+        if (intent.hasExtra("placemark_edit")) {
+            state=1;
+            btnAdd.text = getText(R.string.button_savePlacemark)
+            placemark = intent.extras?.getParcelable<PlacemarkModel>("placemark_edit")!!
+            placemarkTitle.setText(placemark.title)
+            description.setText(placemark.description)
+            placemarkImage.setImageBitmap(readImageFromPath(this, placemark.image))
+        }
+        btnAdd.setOnClickListener() {
+            placemark.title = placemarkTitle.text.toString()
+            placemark.description = description.text.toString()
+
+            if (placemark.title.isEmpty()) {
+                toast(R.string.enter_placemark_title)
+            } else {
+                if (state == 0) {
+                    app.placemarks.create(placemark.copy())
+                } else if (state == 1) {
+                    app.placemarks.update((placemark.copy()))
+                }
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
+            }
+        }
+        chooseImage.setOnClickListener {
+            showImagePicker(this, IMAGE_REQUEST)
+        }
     }
-  }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.cancel_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> {
+                toast("Placemark Cancelled")
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                if (data != null) {
+                    placemark.image = data.getData().toString()
+                    placemarkImage.setImageBitmap(readImage(this, resultCode, data))
+                }
+            }
+        }
+    }
 }
+
+
